19ac0a2e8632c315a71aa4991d00e24520ec8fc8
Sriharsh Dubale <dsriharsh@gmail.com>
2021-11-12 17:39:19 +0530

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/listing">
+          <h1>listing</h1>
+        </Route>
+        <Route path="/details">
+          <h1>details</h1>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
