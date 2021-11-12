02b4fb8fee86d69785872e5fc69c4fd46e28f35f
Sriharsh Dubale <dsriharsh@gmail.com>
2021-11-12 22:35:55 +0530

    listing page

---
+++ src/pages/ListingPage/ListingPage.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import SearchBox from "../../components/SearchBox/SearchBox";
+import { IJob } from "../../interfaces/job";
+import { getAllJobs } from "../../services/services";
+
+function ListingPage() {
+  const [jobs, setJobs] = useState<IJob[]>([]);
+
+  useEffect(() => {
+    getAllJobs()
+      .then((jobs: IJob[]) => {
+        console.log("jobs", jobs);
+        console.log(
+          jobs.reduce((accumulator: any, currentValue, currentIndex, array) => {
+            return accumulator.push(currentValue.applyUrl);
+          }, [])
+        );
+      })
+      .catch((err) => {});
+  }, []);
+
+  return (
+    <div>
+      <SearchBox />
+    </div>
+  );
+}
+
+export default ListingPage;
