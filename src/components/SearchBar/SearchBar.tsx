2f0f475ceaa5091be2e863b0a8ebd8839ff278cb
Sriharsh Dubale <dsriharsh@gmail.com>
2021-11-13 16:19:52 +0530

    search bar component

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,19 +1,54 @@
+import Spinner from "../icons/Spinner";
 import SearchIcon from "./SearchIcon";
+interface IProps {
+  loading?: boolean;
+  options?: { value: string }[];
+  onChange?: (e: string) => void;
+  onSubmit?: (e: string) => void;
+}
 
-function SearchBar() {
+function SearchBar({
+  options = [],
+  onChange,
+  onSubmit,
+  loading = false,
+}: IProps) {
+  const onSubmitHandler = (e: any) => {
+    e.preventDefault();
+    onSubmit && onSubmit(e.target[0].value);
+  };
+  const onChangeHandler = (e: any) => {
+    onChange && onChange(e.currentTarget[0].value);
+  };
+
   return (
-    <div className="input-group mb-3">
-      <input
-        type="text"
-        className="form-control"
-        placeholder="Username"
-        aria-label="Username"
-        aria-describedby="basic-addon1"
-      />
-      <span className="input-group-text" id="basic-addon1">
-        <SearchIcon />
-      </span>
-    </div>
+    <form
+      className="form-horizontal"
+      onSubmit={onSubmitHandler}
+      onChange={onChangeHandler}
+    >
+      <div className="input-group mb-3 w-100">
+        <input
+          className="form-control"
+          list="datalistOptions"
+          id="search"
+          placeholder="Type to search..."
+        />
+        <datalist id="datalistOptions">
+          {options.map((option, i) => (
+            <option value={option.value} key={i} />
+          ))}
+        </datalist>
+        <button
+          className="btn btn-outline-secondary"
+          type="submit"
+          id="button-addon2"
+        >
+          {/* {loading && <Spinner />} */}
+          <SearchIcon />
+        </button>
+      </div>
+    </form>
   );
 }
 
