37f617791caedaa5f965463a6752d435cc52eaf6
Sriharsh Dubale <dsriharsh@gmail.com>
2021-11-14 13:45:06 +0530

    services url modified

--- src/services/services.ts
+++ src/services/services.ts
@@ -7,20 +7,20 @@ export enum LookUp {
 }
 
 export const getAllJobs = async () =>
-  api.get("/api/v1/jobs").then(extractStandardResponseData);
+  api.get("/api/v2/jobs").then(extractStandardResponseData);
 
 export const getJob = async () =>
-  api.get("/api/v1/jobs/42245").then(extractStandardResponseData);
+  api.get("/api/v2/jobs/42245").then(extractStandardResponseData);
 
 export const getLookUps = async (lookup: LookUp) =>
-  api.get(`/api/v1/${lookup}`).then(extractStandardResponseData);
+  api.get(`/api/v2/${lookup}`).then(extractStandardResponseData);
 
 export const getJobByFilter = async (
   q: string,
-  loc?: string,
-  dept?: string,
-  fun?: string
+  loc?: number | null,
+  dept?: number | null,
+  fun?: number | null
 ) =>
   api
-    .get(`/api/v1/jobs`, { params: { q, loc, dept, fun } })
+    .get(`/api/v2/jobs`, { params: { q, loc, dept, fun } })
     .then(extractStandardResponseData);
