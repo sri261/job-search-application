13c9f4de534867dd80003830e8b1f297523e7ff5
Sriharsh Dubale <dsriharsh@gmail.com>
2021-11-14 16:16:09 +0530

    search box modified

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
+import { useHistory, useLocation } from "react-router";
 import { IGeneral, IJob } from "../../interfaces/job";
-import { getJobByFilter, getLookUps, LookUp } from "../../services/services";
-import { consoleLog } from "../../utils/utils";
+import { getJobs, getLookUps, LookUp } from "../../services/services";
 import CancelIcon from "../icons/CancelIcon";
 import SearchBar from "../SearchBar/SearchBar";
 import SelectDropDown from "../SelectDropDown/SelectDropDown";
 
 interface IProps {
   onChange?: (jobs: IJob[]) => void;
+  isLoading?: (loading: boolean) => void;
 }
 
 interface IFilters {
@@ -16,7 +17,8 @@ interface IFilters {
   $function: IGeneral;
 }
 
-function SearchBox({ onChange }: IProps) {
+function SearchBox({ onChange, isLoading }: IProps) {
+  const location = useLocation();
   const [loading, setLoading] = useState(true);
   const [departments, setDepartments] = useState<IGeneral[]>([]);
   const [locations, setLocations] = useState<IGeneral[]>([]);
@@ -24,31 +26,41 @@ function SearchBox({ onChange }: IProps) {
   const [searchOptions, setSearchOptions] = useState<
     { value: string; id: number }[]
   >([]);
-
   const [filters, setFilters] = useState<IFilters>({
     department: {} as IGeneral,
     location: {} as IGeneral,
     $function: {} as IGeneral,
   });
+  const history = useHistory();
+  const isLoadingHandler = (loading: boolean) => {
+    isLoading && isLoading(loading);
+  };
 
-  const searchHandler = (e: string) => {
-    console.log(filters);
-    consoleLog("Search Api Call");
-    getJobByFilter(
+  const searchHandler = (e?: string) => {
+    isLoadingHandler(true);
+    const locationState = location.state as IFilters;
+    getJobs(
       e,
-      filters.location.id,
-      filters.department.id,
-      filters.$function.id
+      locationState?.location?.id || null,
+      locationState?.department?.id || null,
+      locationState?.$function?.id || null
     )
       .then((jobs: IJob[]) => {
-        consoleLog("Search Api Response =>>>>", jobs);
         onChange && onChange(jobs);
+        isLoadingHandler(false);
         setSearchOptions(
           jobs.map((job: IJob) => ({ value: job.title, id: job.id }))
         );
       })
-      .catch((err) => {});
+      .catch((err) => {
+        isLoadingHandler(false);
+      });
   };
+
+  useEffect(() => {
+    searchHandler();
+    setFilters(location.state as IFilters);
+  }, [location.state]);
 
   useEffect(() => {
     Promise.all([
@@ -73,19 +85,25 @@ function SearchBox({ onChange }: IProps) {
         <div className="col"></div>
         <div className="row">
           <SearchBar
-            onChange={searchHandler}
+            onChange={(e) => {
+              if (e.length === 0) {
+                searchHandler();
+              }
+            }}
             onSubmit={searchHandler}
             options={searchOptions}
-            loading={true}
           />
         </div>
         <div className="row">
           <div className="col-4">
             <SelectDropDown
               onChange={(e: any) => {
-                setFilters({
-                  ...filters,
-                  department: JSON.parse(e.currentTarget.value),
+                history.push({
+                  pathname: "/listing",
+                  state: {
+                    ...(location.state as typeof location),
+                    department: JSON.parse(e.currentTarget.value),
+                  },
                 });
               }}
               options={departments}
@@ -96,9 +114,12 @@ function SearchBox({ onChange }: IProps) {
           <div className="col-4">
             <SelectDropDown
               onChange={(e: any) => {
-                setFilters({
-                  ...filters,
-                  location: JSON.parse(e.currentTarget.value),
+                history.push({
+                  pathname: "/listing",
+                  state: {
+                    ...(location.state as typeof location),
+                    location: JSON.parse(e.currentTarget.value),
+                  },
                 });
               }}
               options={locations}
@@ -109,9 +130,12 @@ function SearchBox({ onChange }: IProps) {
           <div className="col-4">
             <SelectDropDown
               onChange={(e: any) => {
-                setFilters({
-                  ...filters,
-                  $function: JSON.parse(e.currentTarget.value),
+                history.push({
+                  pathname: "/listing",
+                  state: {
+                    ...(location.state as typeof location),
+                    $function: JSON.parse(e.currentTarget.value),
+                  },
                 });
               }}
               options={functions}
@@ -130,10 +154,12 @@ function SearchBox({ onChange }: IProps) {
                   {filters[key as keyof IFilters].title}
                   <CancelIcon
                     onClick={() => {
-                      console.log(key);
-                      setFilters({
-                        ...filters,
-                        [key]: {},
+                      history.push({
+                        pathname: "/listing",
+                        state: {
+                          ...(location.state as typeof location),
+                          [key]: {},
+                        },
                       });
                     }}
                   />
@@ -146,10 +172,9 @@ function SearchBox({ onChange }: IProps) {
           <p
             className="h6 text-success cursor_pointer"
             onClick={() => {
-              setFilters({
-                department: {} as IGeneral,
-                location: {} as IGeneral,
-                $function: {} as IGeneral,
+              history.push({
+                pathname: "/listing",
+                state: {},
               });
             }}
           >
